84967b18b61adc1a2b19b7a46f676d9939831ff3
LUÍS THIAGO NOGUEIRA COELHO <33508988821@info.local>
2026-03-04 21:59:47 -0300

    pi

--- senaimath.py
+++ senaimath.py
@@ -4,3 +4,13 @@ def fatorial(numero):
         valor *= numero - 1
         numero -= 1
     return valor
+
+
+def pi(n=10000000):
+    c = 0
+    for i in range (n):
+        a = (-1)**i
+        b = (2*i)+1
+        c += a/b
+    resultado = 4*(c)
+    return resultado
